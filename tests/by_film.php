2c2453b22a010080de861a40d16d10223eb55eaa
M1ke <inferno.m1ke@gmail.com>
2012-10-27 20:54:36 +0100

    Written tests for loading JSON

--- tests/by_film.php
+++ tests/by_film.php
@@ -2,11 +2,30 @@
 include '../processing.php';
 
 class ByFilm extends PHPUnit_Framework_TestCase {
+	private $type='films';
+
+	// these are only needed for testing, as real class will have these passed in constructor or method calls
+	private function getJSON($file) {
+		$file.='.json';
+		$fh=fopen($file,'r');
+		if (!empty($fh)) {
+			$json=fread($fh,filesize($file));
+			fclose($fh);
+		}
+		return $json;
+	}
+	private function getJSONFormatted($file) {
+		$json=$this->getJSON($file);
+		return '"'.$json.'"';
+	}
+
 	public function testLoadsJSONFile () {
-		$json='"'.include 'films.json'.'"';
+		$json=$this->getJSON($this->type);
 		$this->assertTrue(!empty($json));
 	}
-	public function testOneFilmNoCommonBirthdays () {
-		$json='"'.include 'films.json'.'"';
+	public function testValidJSON () {
+		$json=$this->getJSON($this->type);
+		$array=json_decode($json);
+		$this->assertTrue(!empty($array));
 	}
 }

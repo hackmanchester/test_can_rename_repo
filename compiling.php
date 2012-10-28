47325e99ce5e06140e1f78d5c670dfb03d9e55b9
M1ke <inferno.m1ke@gmail.com>
2012-10-28 12:02:16 +0000

    Tests for compiler-controller

--- compiling.php
+++ compiling.php
@@ -1,5 +1,37 @@
 <?php
 
-class compiling {
-	
+class Controller {
+	public $output;
+	public $data=array();
+
+	function __construct($request) {
+		switch (true) {
+			case isset($request['actor']):
+				$this->getBirthdaysByActor($request['actor']);
+			break;
+			case isset($request['date']):
+				$this->getBirthdaysByDate($request['date']);
+			break;
+			case isset($request['film']):
+				$this->getBirthdaysByFilm($request['film']);
+			break;
+			default:
+				$output=false;
+		}
+	}
+
+	private function getBirthdaysByActor($actor) {
+		$this->output='actor';
+		return;
+	}
+	private function getBirthdaysByDate($date) {
+		$this->output='date';
+		return;
+	}
+	private function getBirthdaysByFilm($film) {
+		$this->output='film';
+		return;
+	}
+
+
 }
--- tests/compile.php
+++ tests/compile.php
@@ -1,5 +1,88 @@
 <?php
+include '../compiling.php';
 
-class cotrollerTests extends PHPUnit_Framework_TestCase {
-	
+class ControllerTests extends PHPUnit_Framework_TestCase {
+	protected $date_input=array('date'=>'05-21');
+	protected $film_input=array('film'=>'The Matrix');
+	protected $actor_input=array('actor'=>'Clint Eastwood');
+	private function _is_2d_array($arr) {
+		return is_array(reset($data));
+	}
+
+	// setup and routing tests
+	public function testNoInputReturnsBlank () {
+		$controller=new Controller($_GET);
+		$this->assertEquals($controller->output,false);
+	}
+	public function testDateInputChoosesDate() {
+		$controller=new Controller($date_input);
+		$this->assertEquals($controller->output,'date');
+	}
+	public function testFilmInputChoosesFilm() {
+		$controller=new Controller($film_input);
+		$this->assertEquals($controller->output,'film');
+	}
+	public function testActorInputChoosesActor() {
+		$controller=new Controller($actor_input);
+		$this->assertEquals($controller->output,'actor');
+	}
+
+	public function testDateInputReturnsActorsAfterFirstStage() {
+		$controller=new Controller($date_input);
+		$data=$controller->data['actors'];
+		$this->assertTrue(!empty($data));
+		$this->assertTrue(is_array($data));
+	}
+	public function testDateInputReturnsFilmsAfterSecondStage() {
+		$controller=new Controller($date_input);
+		$data=$controller->data['actors_films'];
+		$this->assertTrue(!empty($data));
+		$this->assertTrue(is_array($data));
+		$this->assertTrue(_is_2d_array($data));
+	}
+	public function testDateInputStartsDateProcessor() {
+		$controller=new Controller($date_input);
+		$this->assertTrue($controller->processor instanceof dateProcessing);
+	}
+
+	public function testFilmInputReturnsActorsAfterFirstStage() {
+		$controller=new Controller($film_input);
+		$data=$controller->data['actors'];
+		$this->assertTrue(!empty($data));
+		$this->assertTrue(is_array($data));
+	}
+	public function testFilmInputReturnsDatesAfterSecondStage() {
+		$controller=new Controller($date_input);
+		$data=$controller->data['actors_dates'];
+		$this->assertTrue(!empty($data));
+		$this->assertTrue(is_array($data));
+		$this->assertTrue(_is_2d_array($data));
+	}
+	public function testFilmInputStartsFilmProcessor() {
+		$controller=new Controller($date_input);
+		$this->assertTrue($controller->processor instanceof filmProcessing);
+	}
+
+	public function testActorInputReturnsDateAfterFirstStage() {
+		$controller=new Controller($actor_input);
+		$data=$controller->data['date'];
+		$this->assertTrue(!empty($data));
+	}
+	public function testActorInputReturnsActorsAfterSecondStage() {
+		$controller=new Controller($actor_input);
+		$data=$controller->data['actors'];
+		$this->assertTrue(!empty($data));
+		$this->assertTrue(is_array($data));
+	}
+	public function testActorInputReturnsFilmsAfterThirdStage() {
+		$controller=new Controller($actor_input);
+		$data=$controller->data['actors_films'];
+		$this->assertTrue(!empty($data));
+		$this->assertTrue(is_array($data));
+		$this->assertTrue(_is_2d_array($data));
+	}
+	public function testActorInputStartsActorProcessor() {
+		$controller=new Controller($actor_input);
+		$this->assertTrue($controller->processor instanceof actorProcessing);
+	}
 }

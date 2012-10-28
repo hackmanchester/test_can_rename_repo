67b288d7edd7dc0706c8f7818240341e6af6fa4a
M1ke <inferno.m1ke@gmail.com>
2012-10-28 13:27:10 +0000

    Wrote a new parser for the performance data, all that time on the stupid film-actors! Least I learned some cool mapping Ruby

--- parsing/film-actors.rb
+++ parsing/film-actors.rb
@@ -12,17 +12,17 @@ def films
 	@films
 end
 
-n=0
+# n=0
 CSV.foreach("../data/actor.tsv",{:col_sep=>"\t", :headers=>true, :return_headers => true, :header_converters => :symbol, :converters => :all}) do |row|
 	actor = Hash[row.headers[0..-1].zip(row.fields[0..-1])]
 	if actor[:film]!=nil then
 		actors << {:name => actor[:name] , :films => actor[:film].split(',')}
 		# puts n
-		n+=1
+		# n+=1
 	end
 end
 
-n=0
+# n=0
 CSV.foreach("../data/film.tsv",{:col_sep=>"\t", :headers=>true, :return_headers => true, :header_converters => :symbol, :converters => :all}) do |row|
 	film = Hash[row.headers[0..-1].zip(row.fields[0..-1])]
 	films[film[:id]] = film[:name]
@@ -36,15 +36,3 @@ File.open('films-hash', 'w') {|f| f.write(films) }
 
 actors.each{|actor| actor[:films]=actor[:films].map{|v| films[v]}}
 File.open('final-actors', 'w') {|f| f.write(actors) }
-
-# @conn = Mongo::Connection.new('localhost', 27017, :safe => true)
-# @db   = @conn['sample-db']
-# @coll = @db['test']
-
-# @coll.remove
-# 3.times do |i|
-#   @coll.insert({'a' => i+1})
-# end
-
-# puts "There are #{@coll.count} records. Here they are:"
-# @coll.find.each { |doc| puts doc.inspect }
---
+++ parsing/performance.rb
@@ -0,0 +1,20 @@
+require 'csv'
+require 'rubygems'
+require 'mongo'
+
+@performances=[]
+def performances
+	@performances
+end
+
+n=0
+CSV.foreach("../data/performance.tsv",{:col_sep=>"\t", :headers=>true, :return_headers => true, :header_converters => :symbol, :converters => :all}) do |row|
+	performance = Hash[row.headers[0..-1].zip(row.fields[0..-1])]
+	unless performance[:actor]==nil or performance[:film]!=nil then
+		performances << {:actor => performance[:actor] , :film => performance[:film]}
+		puts n
+		n+=1
+	end
+end
+
+File.open('performances', 'w') {|f| f.write(performances) }
